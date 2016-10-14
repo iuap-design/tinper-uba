c2c156fbd426fd13f4863387dd725c682462225e
Kvkens <kvkens@qq.com>
2016-10-14 15:48:07 +0800

    update:init、list模块化.

--- bin/uba.js
+++ bin/uba.js
@@ -9,18 +9,13 @@
 
 'use strict';
 
-const fs = require('fs-extra');
 const path = require('path');
 const chalk = require('chalk');
-const download = require('download-git-repo');
-const request = require('request')
 const spawn = require('cross-spawn');
 const argv = require('minimist')(process.argv.slice(2));
-const pathExists = require('path-exists');
 const commands = argv._;
 const currentPath = path.resolve('.');
 const help = require('../lib/help');
-
 
 
 if (commands.length === 0) {
@@ -32,57 +27,14 @@ if (commands.length === 0) {
     process.exit(0);
 }
 
-
 switch (commands[0]) {
     case 'init':
-        var name = null,
-            template = null;
-        if (commands[1]) {
-            template = commands[1];
-            name = commands[2];
-        } else {
-            help.help();
-        }
-
-        var root = path.resolve(name);
-        if (!pathExists.sync(name)) {
-            fs.mkdirSync(root);
-        } else {
-            console.log(chalk.red(`当前文件夹 ${name}  已经存在了，当前操作终止.`));
-            process.exit(1);
-        }
-        console.log(chalk.yellow(`开始下载 ${template} 请稍等.`));
-        download(help.getInitGithubRepo(template), `${name}`, function(err) {
-            if (!err) {
-                console.log(chalk.cyan(`最佳实践 ${name} 创建完毕.请执行 'cd ${name} && npm install' 进行安装.`));
-            } else {
-                console.log(chalk.red(`最佳实践下载失败，请检查最佳实践名字是否正确或网络错误.`));
-            }
-        });
+        const init = require('../lib/init');
+        init(commands);
         break;
     case 'list':
-        request({
-            url: 'https://api.github.com/users/uba-templates/repos',
-            headers: {
-                'User-Agent': 'uba'
-            }
-        }, function(err, res, body) {
-            if (err) console.log(err);
-            var requestBody = JSON.parse(body);
-            if (Array.isArray(requestBody)) {
-                console.log('  Available official templates:');
-                console.log();
-                requestBody.forEach(function(repo) {
-                    console.log(
-                        '  ' + chalk.yellow('★') +
-                        '  ' + chalk.blue(repo.name) +
-                        ' - ' + repo.description)
-                });
-                console.log();
-            } else {
-                console.error(requestBody.message);
-            }
-        });
+        const list = require('../lib/list');
+        list();
         break;
     case 'server':
         var port = 3000;
